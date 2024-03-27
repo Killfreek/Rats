c529c17716d8301f023c77bef8b189613e40aaed
r <rm>
2024-03-27 17:24:57 +0000

    change spawning enemy from node to packed scene reference

--- EnemySpawnController.gd
+++ EnemySpawnController.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 @export var spawnLocations : Array[Node2D]
-@export var enemy : Node2D
+@export var enemyScene : PackedScene
 
 func _on_enemy_spawn_timer_timeout():
+	print("spawning enemy");
 	var rng = RandomNumberGenerator.new()
 	var spawnLocation = spawnLocations[rng.randf_range(0, spawnLocations.size() - 1)];
 	spawnLocation.position.x -= 100;
-	var enemy = enemy.new();
+	var enemy = enemyScene.instantiate();
 	enemy.position = spawnLocation.position;
+	print("enemy spawned");
+
+	add_child(enemy);
