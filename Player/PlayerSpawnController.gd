9dc09d5185afc835d74a7ec20e89991027b23c50
r <rm>
2024-04-15 17:53:23 +0100

    Added a player spawn controller
    player spawn controller has been added that can be used to re spawn the player

---
+++ Player/PlayerSpawnController.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@export var player : PackedScene
+@export var playerSpawnLocation : Vector2
+
+@export var PlayerAreaTopLeftCorner : Node2D
+@export var PlayerAreaBottomRightCorner : Node2D
+
+var canSpawn : bool
+
+func _ready():
+	canSpawn = false;
+
+func _process(delta):
+	if Input.is_action_pressed("SpawnPlayer") && canSpawn:
+		canSpawn = false;
+		var player = player.instantiate();
+		player.PlayerAreaTopLeftCorner = PlayerAreaTopLeftCorner;
+		player.PlayerAreaBottomRightCorner = PlayerAreaBottomRightCorner;
+		player.position = playerSpawnLocation;
+		add_child(player);
