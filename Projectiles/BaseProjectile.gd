6992914cf88412bf4af95a6b684e862f26f58f0d
r <rm>
2024-05-27 12:34:12 +0100

    Projectiles no longer kill each other

--- Projectiles/BaseProjectile.gd
+++ Projectiles/BaseProjectile.gd
@@ -8,6 +8,9 @@ var finalPosition : Vector2
 @export var damage : int
 
 func _on_area_2d_area_entered(area):
+	print(area.get_name());
 	if(area.get_name() == "Player"):
 		area.takeDamage(damage);
-	queue_free();
+	
+	if(area.get_name() != "Area2D"):
+		queue_free();
