5fd71cc0439d43a11a1dc368c5181183baae0122
r <rm>
2024-07-10 18:58:29 +0100

    can now use escape to open and close rather than just open menu

--- GUIScripts/InGameMenu.gd
+++ GUIScripts/InGameMenu.gd
@@ -1,11 +1,16 @@
 extends Node
 
+var menuShown : bool
+
 func _ready():
 	hideMenu();
 
 func _process(delta):
 	if Input.is_action_just_pressed("InGameMenu"):
-		showMenu();	
+		if menuShown:
+			hideMenu();	
+		else:
+			showMenu();
 
 func hideMenu():
 	var playButton = get_node("Play");
@@ -17,6 +22,8 @@ func hideMenu():
 	exitButton.disabled = true;
 	
 	get_tree().paused = false;
+	
+	menuShown = false;
 
 func showMenu():
 	var playButton = get_node("Play");
@@ -28,6 +35,8 @@ func showMenu():
 	exitButton.disabled = false;
 	
 	get_tree().paused = true;
+	
+	menuShown = true;
 	
 func _on_exit_pressed():
 	get_tree().change_scene_to_file("res://Levels/StartMenu.tscn");
