f1778c50b278077b1d1089a9a4c0e66fba166654
r <rm>
2024-07-10 18:34:50 +0100

    removed play button from in game menu

--- GUIScripts/InGameMenu/Listener.gd
+++ GUIScripts/InGameMenu/Listener.gd
@@ -3,10 +3,6 @@ extends Node
 var menuOpen : bool
 
 func _ready():
-	var playButton = get_node("Play");
-	playButton.hide();
-	playButton.disabled = true;
-	
 	var exitButton = get_node("Exit");
 	exitButton.hide();
 	exitButton.disabled = true;
@@ -16,20 +12,12 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("InGameMenu"):
 		if !menuOpen:
-			var playButton = get_node("Play");
-			playButton.show();
-			playButton.disabled = false;
-			
 			var exitButton = get_node("Exit");
 			exitButton.show();
 			exitButton.disabled = false;
 			
 			menuOpen = true;			
-		else:
-			var playButton = get_node("Play");
-			playButton.hide();
-			playButton.disabled = true;
-			
+		else:			
 			var exitButton = get_node("Exit");
 			exitButton.hide();
 			exitButton.disabled = true;
--- GUIScripts/InGameMenu/Play.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func _on_pressed():
-	get_tree().paused = false;
