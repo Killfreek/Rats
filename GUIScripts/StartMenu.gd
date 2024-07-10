7a6a671194dcca722cd416abe4358b17a604381e
r <rm>
2024-07-10 19:01:47 +0100

    Replaced main menu script with a 1 instead of 1 per button

---
+++ GUIScripts/StartMenu.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _on_exit_pressed():
+	get_tree().quit();
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_play_pressed():
+	pass # Replace with function body.
