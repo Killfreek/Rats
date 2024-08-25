94b8ed69f6fe73652676164baaf31eadf908f08e
r <rm>
2024-08-25 16:05:35 +0100

    board creation now sues both light and dark pattern

--- Scripts/LevelCreator.gd
+++ Scripts/LevelCreator.gd
@@ -12,10 +12,27 @@ func _ready():
 func setUpBoard(rows, columns):
 	tileMap.clear();
 	
-	var pattern = tileMap.tile_set.get_pattern(1);
+	# board pattern
+	var useLightPattern = true;
+	var lightPattern = tileMap.tile_set.get_pattern(0);
+	var darkPattern = tileMap.tile_set.get_pattern(1);
 	for row in range(rows):
 		for col in range(columns):
-			tileMap.set_pattern(0, tileMap.local_to_map(Vector2(col * 100, row * 100)), pattern);
+			if useLightPattern:
+				tileMap.set_pattern(0, tileMap.local_to_map(Vector2(col * 100, row * 100)), lightPattern);
+				useLightPattern = false;
+			else:
+				tileMap.set_pattern(0, tileMap.local_to_map(Vector2(col * 100, row * 100)), darkPattern);
+				useLightPattern = true;
+		if useLightPattern:
+			useLightPattern = false;
+		else:
+			useLightPattern = true;
+			
+	var borderPattern = tileMap.tile_set.get_pattern(4);
+	for col in range(columns):
+		tileMap.set_pattern(0, tileMap.local_to_map(Vector2(col * 100, -100)), borderPattern);
+		tileMap.set_pattern(0, tileMap.local_to_map(Vector2(col * 100, (rows) * 100)), borderPattern);
 
 func setUpPlayer(position):
 	playerController.playerSpawnLocation = position;
